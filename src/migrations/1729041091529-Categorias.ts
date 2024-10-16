48d46fefd3d8801955536dec6f254362933af427
Nayla Hilana da Silva <hilanacs@outlook.com>
2024-10-15 22:58:27 -0300

    feat: adiciona migrations para Locais

---
+++ src/migrations/1729041091529-Categorias.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class Categorias1729041091529 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categorias',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+          },
+          {
+            name: 'nome',
+            type: 'varchar',
+            length: '100',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categorias');
+  }
+}
