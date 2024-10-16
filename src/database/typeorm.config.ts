b3db24a1f78b944e02cbb207434dc81ccfea1ad7
Nayla Hilana da Silva <hilanacs@outlook.com>
2024-10-15 22:56:21 -0300

    feat: adiciona configuração do typeORM

---
+++ src/database/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+import { Local } from '../locais/local.entity';
+import { Usuario } from '../usuarios/usuario.entity';
+import { Categoria } from '../categorias/categoria.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); 
+
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [Local, Usuario, Categoria],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  synchronize: false,
+});
+
+export default dataSource;
