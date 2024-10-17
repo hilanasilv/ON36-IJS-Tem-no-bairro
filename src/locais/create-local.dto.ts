c4bc3532818044c27870301e377c5728585ab238
Nayla Hilana da Silva <hilanacs@outlook.com>
2024-10-17 18:08:04 -0300

    feat: adiciona o CreateLocalDto

---
+++ src/locais/create-local.dto.ts
@@ -0,0 +1,10 @@
+export class CreateLocalDto {
+    nome: string;
+    descricao: string;
+    categorias: string;
+    bairroId:number;
+    endereco: string;
+    horarioFuncionamento?: string;
+    usuarioNome: string;
+  }
+  
